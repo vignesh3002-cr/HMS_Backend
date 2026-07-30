806a66882f2fc896ee6a398a4069a8d50a8c834e
badcadet <daniel123abraham@gmail.com>
2026-07-30 22:25:58 +0530

    server.ts change

--- src/server.ts
+++ src/server.ts
@@ -26,12 +26,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  })
-);
+app.use(cors());
+
 app.use(cookieParser());
 
 app.use(express.json());
